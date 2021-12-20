9b31590bb8423d7df4b9ab661a9e64b74b94a5e5
Nikita <nikitysik2016@gmail.com>
2021-12-20 14:46:55 +0200

    Added app's base structure.

---
+++ src/shared/Index.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react'
+
+export const Index = () => {
+  return (
+    <Fragment>
+      <h1 className="text-danger text-center">List of phone numbers will be here ...</h1>
+    </Fragment>
+  )
+}
